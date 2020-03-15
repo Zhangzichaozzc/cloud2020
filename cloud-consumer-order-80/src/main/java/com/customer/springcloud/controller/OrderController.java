b2f905846e259108e006bf577b46ebf324935050
Zichao.zhang <zhangzc_zzc@163.com>
2020-03-15 18:42:06 +0800

    test RestTemplate.xxxForEntity() method

--- cloud-consumer-order-80/src/main/java/com/customer/springcloud/controller/OrderController.java
+++ cloud-consumer-order-80/src/main/java/com/customer/springcloud/controller/OrderController.java
@@ -1,6 +1,7 @@
 package com.customer.springcloud.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -20,7 +21,7 @@ import com.customer.springcloud.entity.Payment;
 @RestController
 public class OrderController {
 
-//    private static final String URL_PREFIX = "http://localhost:8001";
+    //    private static final String URL_PREFIX = "http://localhost:8001";
     // 在 主配置类中 启动了 @EnableEurekaClient 后可以通过注册名去访问 服务提供者
     // 在 Spring Cloud Hoxton.RS1 中， 直接 添加 @EnableEurekaClient 不行，会报错，解决 bug 需要在 RestTemplate 组件注册时添加 @LoadBalanced 注解
     private static final String URL_PREFIX = "http://CLOUD-PAYMENT-SERVICE";
@@ -42,5 +43,22 @@ public class OrderController {
     @GetMapping("/consumer/ping")
     public CommonResult<String> getById() {
         return restTemplate.getForObject(URL_PREFIX + "/ping", CommonResult.class);
+    }
+
+    /**
+     * 测试 RestTemplate.getForEntity() 方法的使用
+     *
+     * @param id
+     * @return
+     */
+    @GetMapping("/consumer/entity/payment/{id}")
+    public ResponseEntity<CommonResult> getEntityById(@PathVariable("id") Long id) {
+        ResponseEntity<CommonResult> entity = restTemplate.getForEntity(URL_PREFIX + "/payment/" + id,
+                CommonResult.class);
+        if (entity.getStatusCode().is2xxSuccessful()) {
+            System.err.println("entity.getHeaders() = " + entity.getHeaders());
+            return entity;
+        }
+        return null;
     }
 }
